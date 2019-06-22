f1ffacd7db7e45f6c72e7dfa78db2ed9c5a07770
ircube <ivo753@gmail.com>
2019-06-22 10:12:59 -0400

    list users, add react-image.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Img from 'react-image';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: []
+  }
+  // [...]
+  async componentDidMount() {
+    fetch('https://api.github.com/users')
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({ users: data })
+      })
+      .catch(console.log)
+  }
+
+  render() {
+    // const imgUrl =v; 
+    return (
+      <div className="container">
+        <div className="col-xs-12">
+          <h1>Users</h1>
+          {this.state.users.map((user) => (
+            <div className="card" key={user.login}>
+              <Img src={user.avatar_url} alt={user.avatar_id} height="100" width="100" />
+              <div className="card-body">
+                <h5 className="card-title">{user.login}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  {user.site_admin &&
+                    <span>
+                      Admin
+                    </span>
+                  }
+                  {!user.site_admin &&
+                    <span>
+                      No admin
+                </span>
+                  }
+                </h6>
+
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
